639a5dd90360b805cab3cada1884f3a2d69564e3
ggggggggithub <hamtoli5@gmail.com>
2024-07-30 23:39:18 +0900

    feat project

--- src/app/Components/project.tsx
+++ src/app/Components/project.tsx
@@ -1,8 +1,8 @@
 export default function Project() {
   return (
     <section id="project" className="container mx-auto p-4 mt-8 px-40">
-      <h1 className="text-4xl font-bold mb-4">Project Section</h1>
-      <p>This is the Project section. Add your content here.</p>
+      <h1 className="text-4xl font-bold mb-4">도와조약</h1>
+      <p>기술 스택 : React, javascript, Redux, Styled-Component</p>
     </section>
   );
 }
