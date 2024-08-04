2884907ca519a05a5be6416044b9e566796a2202
ggggggggithub <hamtoli5@gmail.com>
2024-08-04 23:49:08 +0900

    css main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       <section className="min-h-screen flex flex-col justify-center items-center bg-blue-100 dark:bg-gray-800 text-black dark:text-white">
         <div className="flex flex-col items-center justify-center min-h-screen py-2 space-y-2 leading-none">
           <h1 className="text-4xl md:text-6xl mb-1 text-center slide-in-down-fixed tracking-wider">
-            Hello, I am
+            Hello! I am
           </h1>
           <p className="text-6xl md:text-8xl font-bold mb-1 text-center">
             김성수
@@ -50,6 +50,9 @@ export default function Home() {
           <div className="h-16 md:h-20 flex items-center">
             <p className="text-3xl md:text-4xl font-bold mb-1 text-center">
               {currentText}
+            </p>
+            <p className="text-3xl md:text-4xl font-bold mb-1 text-center">
+              를 공부하는
             </p>
           </div>
         </div>
