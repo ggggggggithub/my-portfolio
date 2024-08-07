e9d8bada3918b0e519d84faf8f3f66516473f5f1
ggggggggithub <hamtoli5@gmail.com>
2024-08-07 23:56:50 +0900

    add project

--- src/app/Components/project.tsx
+++ src/app/Components/project.tsx
@@ -1,10 +1,18 @@
 export default function Project() {
   return (
     <section id="project" className="container mx-auto p-4 mt-8 px-40">
-      <h1 className="text-4xl font-bold mb-4">도와조약</h1>
-      <p>About Project :</p>
-      <p>My Part :</p>
-      <p>기술 스택 : React, javascript, Redux, Styled-Component</p>
+      <div className="project mb-8">
+        <h1 className="text-4xl font-bold mb-4">도와조약</h1>
+        <p>About Project :</p>
+        <p>My Part :</p>
+        <p>기술 스택 : React, JavaScript, Redux, Styled-Component</p>
+      </div>
+      <div className="project mb-8">
+        <h1 className="text-4xl font-bold mb-4">펫칭</h1>
+        <p>About Project :</p>
+        <p>My Part in Project :</p>
+        <p>기술 스택 : Vue, TypeScript, Vuex, TailwindCSS</p>
+      </div>
     </section>
   );
 }
