ec99cf759f8e46c82f9d67e228d62f61348aca74
ggggggggithub <hamtoli5@gmail.com>
2024-08-30 23:51:26 +0900

    css footer

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,14 +2,14 @@ const Footer = () => {
   return (
     <footer className="bg-emerald-500 text-white p-6">
       <div className="container mx-auto text-right">
-        <p>Made By KimSeongSu</p>
         <a
           href="https://github.com/ggggggggithub"
           target="_blank"
           rel="noopener noreferrer"
           className="ml-2 text-black hover:underline"
         >
-          Github
+          Made By <br />
+          Kim Seong Su
         </a>
       </div>
     </footer>
