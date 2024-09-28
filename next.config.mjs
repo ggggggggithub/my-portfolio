17fb777d084a2fc7352dd9264a160ca77dbec787
ggggggggithub <hamtoli5@gmail.com>
2024-09-28 22:34:36 +0900

    github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
 
+const nextConfig = {
+  output: "export", // 정적 사이트로 내보내기
+  assetPrefix: isProd ? "my-portfolio" : "",
+  basePath: isProd ? "my-portfolio" : "",
+};
 export default nextConfig;
